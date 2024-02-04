a51b48862833cd5d25a197807bb18093f1a8474e
mathewpan2 <mathew.pan2@gmail.com>
2024-02-03 23:37:35 -0500

    now listens for messages

--- server/api.py
+++ server/api.py
@@ -1,8 +1,11 @@
 import discord
+import json
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
 
+system_content = "You are Bhi (you're also commonly to referred to B, buh, and zhi), a 20-year-old college student from Rochester, known for your laid-back and sloth-like demeanor. Your favorite pastime involves lounging on your couch, playing League of Legends for hours on end. You don't like your friends and frequently refuse to play games or hang out with them, unless they also attenteded stuyvesant high school."
 client = discord.Client(intents=intents)
 
 @client.event
@@ -15,8 +18,36 @@ async def on_message(message):
         return
     
     if message.channel.id == 542879446655434752: # rit
-        pass
+        data = { "messages": 
+            [
+                {"role": "system", "content": system_content},
+                {"role": "user", "content": message.content},
+            ]
+         }
+
+        data = json.dumps(data)
+        content = requests.post('http://localhost:8080/model/chat', data=data)
+        load = content.json()
+        send = load['choices'][0]['message']['content'] # have to use this to get message content
+        await message.channel.send(send)
+
 
     if message.channel.id == 983138529775341650: # test
-        pass
+        data = { "messages": 
+            [
+                {"role": "system", "content": system_content},
+                {"role": "user", "content": message.content},
+            ]
+         }
+
+        data = json.dumps(data)
+        content = requests.post('http://localhost:8080/model/chat', data=data)
+        load = content.json()
+        send = load['choices'][0]['message']['content'] # have to use this to get message content
+        await message.channel.send(send)
+
+        
+
+
+client.run()
     
